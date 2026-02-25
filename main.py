5435b5b9a72bf55f1ce4fb80c7501527ed734d37
mulagaroganataya <tvm5403@gmail.com>
2026-02-25 21:15:22 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+
+def main() -> None:
+    root = tk.Tk()
+    root.title("Snake")
+    root.geometry("600x480")
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
